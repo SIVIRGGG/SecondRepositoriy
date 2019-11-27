428fe7c4056ddc532f835a3f8b720925e22dd15f
sivirggg <sivirggg@gmail.com>
2019-11-27 13:46:48 +0300

    Не работают кнапки

--- js.js
+++ js.js
@@ -1,235 +1,220 @@
 var state = {
     currentSong: null,
+    lastSong: null,
+    songListId: 'current_song_list',
     music: [{
-        id: 1,
-        imageSrc: 'img/jara.jpg',
-        buttonId: 'zhara1',
-        audioId: 'audio1',
-        audioSrc: 'audio/Elena_Temnikova_-_ZHara.mp3',
-        name: 'Жара',
-        artist: 'Елена Темникова',
-        time: '5:38'
-    }]
+            id: 1,
+            imageSrc: 'img/jara.jpg',
+            buttonId: 'gdgdfgdf',
+            audioId: 'audiogggffdfg1',
+            audioSrc: 'audio/Elena_Temnikova_-_ZHara.mp3',
+            name: 'Жара',
+            artist: 'Елена Темникова',
+            time: '5:38'
+        },
+        {
+            id: 2,
+            imageSrc: 'img/senorita.jpg',
+            buttonId: 'zhadfgfdfggra2',
+            audioId: 'g',
+            audioSrc: 'audio/Shawn Mendes & Camila Cabello - Senorita.mp3',
+            name: 'Senorita',
+            artist: 'Shawn Mendes',
+            time: '3:29'
+        },
+        {
+            id: 3,
+            imageSrc: 'img/ashiel.jpg',
+            buttonId: 'zhadfggfdgfdra3',
+            audioId: 'gfdf',
+            audioSrc: 'audio/mixvon.net_ashley_o_-_on_a_roll_imp416438.mp3',
+            name: 'On A Rol',
+            artist: 'Ashley O',
+            time: '15:56'
+        },
+        {
+            id: 4,
+            imageSrc: 'img/egor.jpg',
+            buttonId: 'gjhhg',
+            audioId: 'audio423dsd4234',
+            audioSrc: 'audio/egor_krid_-_samaya_samaya_(zaycev.net).mp3',
+            name: 'Samaya Samaya',
+            artist: 'Egor Krid',
+            time: '3:56'
+        }
+
+    ]
 };
 
 
-document.getElementById('zhara1').onclick = function() {
-    playOrPause(1);
-};
+document.getElementById('playId').onclick = function() {
+    play();
+}
+document.getElementById('prevId').onclick = function() {
+    prev();
+}
+document.getElementById('nextId').onclick = function() {
+    next();
+}
 
-document.getElementById('zhara2').onclick = function() {
-    playOrPause(2);
-};
+function play() {
+    if (state.currentSong == null && state.lastSong == null) {
+        playOrPause(state.music[0].audioId);
+        return;
+    }
 
-function playOrPause(id) {
-    let myAudio = document.getElementById("audio" + id);
+    if (state.currentSong != null) {
+        playOrPause(state.currentSong);
+        return;
+    }
+
+    if (state.currentSong == null && state.lastSong != null) {
+        playOrPause(state.lastSong);
+        return;
+    }
+}
+
+
+
+function next() {
+    if (state.currentSong == null && state.lastSong == null) {
+        return;
+    }
+
+    if (state.lastSong != null && state.lastSong != state.music[state.music.length - 1].audioId) {;
+
+        let i = state.music.forEach(function(audioId) {
+            let s = audioId;
+            console.log(s);
+            // s.IndexOf(true);
+            // playOrPause(state.music.IndexOf().audioId);
+        });
+        playOrPause(s[0]);
+        // let so = state.music.includes(audioId = true);
+        // console.log(so + 1);
+
+        // должен пробежаться по массиву, определить на каком треке сейчас находимся и перейти к следующему!
+        return;
+    }
+    if (state.lastSong != null && state.lastSong == state.music[state.music.length - 1].audioId) {
+        return;
+    }
+
+    console.log('next');
+}
+
+// next();
+
+function prev() {
+    if (state.currentSong == null && state.lastSong == null) {
+        return;
+    }
+    if (state.lastSong != null && state.lastSong != state.music[state.music.length[0]].audioId) {;
+        // должен пробежаться по массиву, определить на каком треке сейчас находимся и перейти к следующему!
+        return;
+    }
+    if (state.lastSong != null && state.lastSong == state.music[state.music.length[0]].audioId) {
+        return;
+    }
+    console.log('prev');
+}
+// prev();
+
+function playOrPause(audioId) {
+    let myAudio = document.getElementById(audioId);
 
     if (state.currentSong == null) {
-        state.currentSong = id;
+        state.currentSong = audioId;
+        state.lastSong = audioId;
         myAudio.play();
         return;
     }
 
-    if (state.currentSong != null && state.currentSong == id) {
+    if (state.currentSong != null && state.currentSong == audioId) {
         state.currentSong = null;
         myAudio.pause();
         return;
     }
 
-    if (state.currentSong != null && state.currentSong != id) {
-        let myOldAudio = document.getElementById("audio" + state.currentSong);
+    if (state.currentSong != null && state.currentSong != audioId) {
+        let myOldAudio = document.getElementById(state.currentSong);
         myOldAudio.pause();
-        state.currentSong = id;
+        state.currentSong = audioId;
+        state.lastSong = audioId;
         myAudio.play();
         return;
     }
 
 };
 
-function createSongElement(songInfo) {
-    console.log('Pip songInfo:', songInfo);
-    //создание песни и вставка
-}
+function addSongToList(songsInfo, songListId, clear) {
+    const triumfCenterList = document.getElementById(songListId);
 
-createSongElement(state.music[0]);
+    if (clear == true) {
+        triumfCenterList.innerHTML = '';
+    }
 
+    let songs = songsInfo.map(function(song) {
+        return createSongElement(song);
+    });
 
-
-
-function block() {
-    let w = document.createElement('w');
-    w.insertAdjacentHTML('beforeend', '<div id="glav"></div>');
-    w.classList.add('triumf_img_block');
-    q.append(w);
-    console.log('tyr');
-    //q.insertAdjacentHTML('beforeend', '<div id="b"class="bl"></div>');
-    // q.insertAdjacentHTML('beforeend', '<div id="img" class="triumf_img_block"></div>');
-}
-block();
-
-function mapMusic() {
-    state.music.map(function(song) {
-        let img = document.createElement('img');
-        img.insertAdjacentHTML('afterbegin', '<img>');
-        img.classList.add('triumf_img');
-        img.src = song.imageSrc;
-        glav.prepend(img);
+    songs.forEach(song => {
+        triumfCenterList.appendChild(song);
     });
 }
-mapMusic();
 
-// function imgPhoto() {
-//     let img = document.createElement('img');
-//     img.classList.add('triumf_img');
-//     img.className = "triumf_img";
-//     img.innerHTML = state.imageSrc;
-//     img.insertAdjacentHTML('afterbegin', '<img class="triumf_img">');
-//     glav.append(img);
-// }
-// imgPhoto();
+function createSongElement(song) {
+    const wrapperTriumf = document.createElement('div');
+    wrapperTriumf.classList.add('triumf_img_block');
 
-function triumfNameBlock() {
-    let nameBlock = document.createElement('nameBlock');
-    nameBlock.insertAdjacentHTML('beforeend', '<div id="nameBlockId"></div>');
-    nameBlock.classList.add('triumf-name-block');
-    glav.after(nameBlock);
-    console.log('nameBlock');
+    const img = document.createElement('img');
+    img.classList.add('triumf_img');
+    img.src = song.imageSrc;
+    wrapperTriumf.appendChild(img);
+
+    const triumfNameBlock = document.createElement('div');
+    triumfNameBlock.classList.add('triumf-name-block');
+
+
+    const triumfName = document.createElement('div');
+    triumfName.classList.add('triumf-name');
+
+    const inpZhara1 = document.createElement('div');
+    const inpZhara1Input = document.createElement('input');
+    inpZhara1Input.id = song.buttonId;
+    inpZhara1Input.type = 'button';
+    inpZhara1Input.value = 'Play';
+
+    inpZhara1Input.onclick = function() {
+        playOrPause(song.audioId);
+    };
+
+    inpZhara1.appendChild(inpZhara1Input);
+
+    const audioRecord = document.createElement('div');
+    audioRecord.insertAdjacentHTML('afterbegin', `<audio id="${song.audioId}" src="${song.audioSrc}" autostart="false"></audio>`);
+
+    const name1 = document.createElement('p');
+    name1.innerHTML = song.name;
+
+    const artist = document.createElement('div');
+    const artistP = document.createElement('p');
+    artistP.innerHTML = song.artist;
+
+    const times = document.createElement('div');
+    times.classList.add('times');
+    times.innerHTML = song.time;
+
+    triumfName.prepend(audioRecord);
+    triumfName.prepend(inpZhara1);
+    triumfNameBlock.appendChild(triumfName);
+    triumfNameBlock.appendChild(name1);
+    triumfNameBlock.appendChild(artistP);
+    triumfNameBlock.appendChild(artist);
+    wrapperTriumf.appendChild(triumfNameBlock);
+    wrapperTriumf.appendChild(times);
+
+    return wrapperTriumf;
 }
-triumfNameBlock();
 
-function buttonAndName() {
-    state.music.map(function(song) {
-        let btnName = document.createElement('btnName');
-        btnName.insertAdjacentHTML('afterbegin', '<div id="btnNameId"> </div>');
-        btnName.classList.add('triumf-name');
-        nameBlockId.prepend(btnName);
-        console.log('triumf-name');
-
-        let inpZhara1 = document.createElement('inpZhara1');
-        inpZhara1.insertAdjacentHTML('afterbegin', '<input id="zhara1" type="button" value="Play">');
-        // inpZhara1.innerHTML = song.buttonId;
-        btnNameId.prepend(inpZhara1);
-        console.log('inpZhara1');
-
-        let audioRecord = document.createElement('audioRecord');
-        audioRecord.insertAdjacentHTML('afterbegin', '<audio id="audio1" autostart="false"></audio>');
-        audioRecord.src = song.audioSr;
-        btnNameId.append(audioRecord);
-        console.log('audioRecord');
-
-        let name = document.createElement('name');
-        name.insertAdjacentHTML('afterbegin', '<p></p>');
-        name.innerHTML = song.name;
-        btnNameId.append(name);
-        console.log('name');
-
-        let artist = document.createElement('artist');
-        artist.insertAdjacentHTML('beforeend', '<div><p></p></div>');
-        artist.innerHTML = song.artist;
-        nameBlockId.append(artist);
-        console.log('artist');
-
-        let times = document.createElement('times');
-        times.insertAdjacentHTML('beforeend', '<div><p></p></div>');
-        times.classList.add('times');
-        times.innerHTML = song.time;
-        nameBlockId.append(times);
-
-    });
-}
-buttonAndName();
-
-// function inputZhara1() {
-//     let inpZhara1 = document.createElement('inpZhara1');
-//     inpZhara1.insertAdjacentHTML('afterbegin', '<input id="zhara1" type="button" value="Play">');
-//     nameBlockId.prepend(inpZhara1);
-//     console.log('inpZhara1');
-//     let audioRecord = document.createElement('audioRecord');
-//     audioRecord.innerHTML = "";
-//     audioRecord.insertAdjacentHTML('afterbegin', '<audio id="audio1" src="audio/Elena_Temnikova_-_ZHara.mp3" autostart="false"></audio>');
-//     nameBlockId.append(audioRecord);
-//     console.log('audioRecord');
-// }
-// inputZhara1();
-
-
-// function audioRecording() {
-//     let audioRecord = document.createElement('audioRecord');
-//     audioRecord.innerHTML = "";
-//     audioRecord.insertAdjacentHTML('afterbegin', '<audio id="audio1" src="audio/Elena_Temnikova_-_ZHara.mp3" autostart="false"></audio>');
-//     nameBlockId.append(audioRecord);
-//     console.log('audioRecord');
-// }
-// audioRecording();
-
-// function name() {
-//     let name = document.createElement('name');
-//     name.innerHTML = '';
-//     nameBlockId.append(name);
-//     console.log('name');
-// }
-// name();
-
-
-/*function triumfImgBlock() {
-    let img = document.createElement('img');
-    img.insertAdjacentHTML('afterbegin', '<div class="triumf_img_block"></div>');
-    q.prepend(img);
-    console.log('pam')
-    img.insertAdjacentHTML('afterbegin', '<img class="triumf_img " src="img/jara.jpg" alt=" "></div>');
-    q.prepend(img);
-    console.log('pup');
-    // img.innerHTML = "src=\"img/jara.jpg\"";
-    q.prepend(img);
-    console.log('pum');
-}
-triumfImgBlock();
-
-function triumfNameBlock() {
-    let nameBlock = document.createElement('nameBlock');
-    nameBlock.insertAdjacentHTML('beforeend', '<div class="triumf-name-block "></div>');
-    console.log('tum');
-
-}
-triumfNameBlock();
-
-function buttonId() {
-    let btn = document.createElement('btn');
-    btn.insertAdjacentHTML('afterbegin', '<input id="zhara1" type="button" value="Play" />');
-    console.log('op');
-}
-buttonId();
-
-function audioId() {
-    let track = document.createElement('track');
-    track.insertAdjacentHTML('afterbegin', '<audio id="audio1" src="audio/Elena_Temnikova_-_ZHara.mp3" autostart="false"></audio>');
-    console.log('tra');
-}
-audioId();
-
-function name() {
-    let name = document.createElement('name');
-    name.insertAdjacentHTML('afterbegin', '<p id="nameW">Жара</p>');
-    // nameW.className = 'triumf_img_block';
-
-    // nameW.prepend(name);
-}
-name();
-
-function artist() {
-    let arti = document.createElement('arti');
-    arti.insertAdjacentHTML('afterend', '<div><p>Елена Темникова</p></div>');
-    // nameW.className = 'triumf_img_block';
-
-    // nameW.prepend(name);
-}
-name();
-*/
-// console.log(triumfImgBlock(Info));
-
-
-// function triumfNamebBlock(Info) {
-//     let div = document.createElement(Info);
-//     document.body.insertAdjacentHTML('afterend', '<div class="triumf-name-block"></div>')
-//     div.className = 'triumf-name-block';
-// }
+addSongToList(state.music, state.songListId, true);
